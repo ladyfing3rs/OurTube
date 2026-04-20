2c536272497a648934ed8bea41b52ccc17d62990
Prabhat Dubey <prabhatdotdubey@proton.me>
2026-04-20 19:29:01 +0530

    init video processing express tsc boilerplate

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+	nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
+	pkgs = import nixpkgs { config = {}; overlays = [];};
+in
+
+pkgs.mkShellNoCC {
+	packages = with pkgs; [
+		nodejs_24
+	];
+
+shellHook = ''
+	echo "Environment Ready!"
+	echo "With Node.js TypeScript"
+'';
+}
